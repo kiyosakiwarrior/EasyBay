ae147a239178b03bce5834dee7a84e2326525bc9
Nathon Chin <nathonchinster234@gmail.com>
2021-07-03 13:50:57 -0700

    EasyBay

---
+++ script.js
@@ -0,0 +1,18 @@
+function cartalert() {
+    alert('Your Cart is empty!')
+}
+
+function remove() {
+    document.querySelector(".cookie").remove()
+}
+
+var x = document.getElementById("#succulents-1")  
+
+function over(element){
+    x.src = "/images/assets/succulents-1.jpg" 
+    x.alt = ("succulents-2")
+}
+
+function out(element){
+
+}
